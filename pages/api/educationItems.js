abf7c469ed4ee0730262bff18a6778dd3f9352e9
Derek Merrikin <dsmerrikin@gmail.com>
2024-03-08 08:31:57 -0500

    Adding to site timeline for gigs

--- pages/api/educationItems.js
+++ pages/api/educationItems.js
@@ -2,16 +2,28 @@ export default function handler(req, res) {
   res.status(200).json([
     {
       id: 1,
+      company: "Easton Consulting Technologies",
+      title: "Technical Team Lead & Senior Software Engineer",
+      date: "September 2023 - Present",
+      summary: `As the Technical Team Lead, I drive success within a fully remote environment by overseeing the development of 
+      7+ high-revenue-driving web products. My role involves coaching and mentoring the team, ensuring their growth and success. 
+      I lead by example, conducting thorough reviews of Pull Requests (PRs) on Github to maintain the utmost code quality standards. 
+      Operating in an Agile environment, I take responsibility for individual and team tickets, entire epics, and ensure timely responses. 
+      Notable achievements include leading the successful launch of two redesigned websites, with a third on track for completion in March 2023. 
+      Additionally, I have written custom WordPress plugins and served as the go-to person for all technology-related requests from the VP of Technology and the CEO.`,
+    },
+    {
+      id: 2,
       company: "Built Technologies",
       title: "Senior Software Engineer",
-      date: "January 2023 - Present",
+      date: "January 2023 - July 2023",
       summary: `As a React developer within a microservice architecture, I focus on creating efficient and streamlined components. 
             I play a crucial role in elevating the code quality standards of my team through thorough code reviews and constructive feedback. Collaborating within Agile Scrum teams, I work towards delivering high-quality software. 
             My expertise extends to a complex PHP environment, where I provide guidance to the team. Additionally, I improve code coverage by implementing comprehensive unit tests using Jest across the microservice architecture. 
             I ensure compliance with WCAG AA accessibility standards and contribute to continuous deployment and AC changes through a Go CI/CD pipeline.`,
     },
     {
-      id: 2,
+      id: 3,
       company: "DEPT",
       title: "Software Engineer",
       date: "March 2021 - October 2022",
@@ -21,7 +33,7 @@ export default function handler(req, res) {
             I ensured compliance with WCAG AA accessibility standards and contributed to continuous deployment and iterative design changes through a CI/CD pipeline.`,
     },
     {
-      id: 3,
+      id: 4,
       company: "Fresenius Medical Care",
       title: "Senior UI/UX Developer",
       date: "December 2015 - March 2021",
@@ -30,7 +42,7 @@ export default function handler(req, res) {
         Additionally, I resolved security vulnerabilities identified through penetration testing, ensuring compliance with WCAG AA accessibility standards. My collaborative skills were put to use in Agile Scrum teams, contributing to efficient teamwork.`,
     },
     {
-      id: 4,
+      id: 5,
       company: "Fidelity Investments",
       title: "Senior UI/UX Developer",
       date: "July 2014 - November 2015",
@@ -38,7 +50,7 @@ export default function handler(req, res) {
         Compliance with WCAG AA accessibility standards was a priority, and I fostered camaraderie and collaboration within teams to enhance productivity. Additionally, I developed a full-stack MEAN solution for an Executive Vice President, streamlining the management of business data.`,
     },
     {
-      id: 5,
+      id: 6,
       company: "Medullan, Inc.",
       title: "Lead UI/UX Developer",
       date: "June 2013 - May 2014",
@@ -47,7 +59,7 @@ export default function handler(req, res) {
         Additionally, I specialized in creating custom WordPress solutions that were meticulously tailored to cater to the specific requirements of clients.`,
     },
     {
-      id: 6,
+      id: 7,
       company: "Southern New Hampshire University",
       title: "Bachelors of Computer Science",
       date: "2020",
