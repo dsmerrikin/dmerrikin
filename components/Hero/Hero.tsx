bf6c465d3d065363fee46a75c36562812085cc69
Derek Merrikin <derek.merrikin@gmail.com>
2024-10-29 09:27:52 -0400

    Updated resume section and photo

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -3,100 +3,96 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <>
-    <section
-      id='home'
-      className='relative z-10 pt-[150px] lg:pt-[100px] 2xl:flex items-center 2xl:h-[700px]'
-    >
-      <div className='container'>
-        <div className='flex flex-wrap mx-[-16px]'>
-          <div className='w-full lg:w-1/2 self-center px-4'>
-            <div>
-              <h1 className='text-base font-semibold text-primary mb-3'>
-                Hey there 👋 I am
-                <span className='font-bold text-black text-3xl sm:text-3xl md:text-[45px] leading-tight sm:leading-tight md:leading-[50px] block mt-1'>
-                  {" "}
-                  Derek Merrikin{" "}
-                </span>
-              </h1>
-              <h2 className='font-medium text-body-color text-lg md:text-xl mb-5'>
-                Senior
-                <span className='text-black'> Software Engineer </span> |
-                Aspiring <span className='text-black'>Chef</span>
-              </h2>
+      <section
+        id="home"
+        className="relative z-10 pt-[150px] lg:pt-[100px] 2xl:flex items-center 2xl:h-[700px]"
+      >
+        <div className="container">
+          <div className="flex flex-wrap mx-[-16px]">
+            <div className="w-full lg:w-1/2 self-center px-4">
+              <div>
+                <h1 className="text-base font-semibold text-primary mb-3">
+                  Hey there 👋 I am
+                  <span className="font-bold text-black text-3xl sm:text-3xl md:text-[45px] leading-tight sm:leading-tight md:leading-[50px] block mt-1">
+                    {" "}
+                    Derek Merrikin{" "}
+                  </span>
+                </h1>
+                <h2 className="font-medium text-body-color text-lg md:text-xl mb-5">
+                  Senior
+                  <span className="text-black"> Software Engineer </span> |
+                  Aspiring <span className="text-black">Chef</span>
+                </h2>
 
-              <p className='max-w-[470px] font-medium text-body-color text-base leading-relaxed mb-11'>
-                Decade-Long Software Expert Empowering Teams with Inspiring
-                Leadership and Cutting-Edge Technologies
-              </p>
-              <div className='flex items-center'>
-                <a
-                  href='#contact'
-                  className='text-base xs:text-center font-semibold text-white bg-primary py-3 px-8 mr-4 hover:shadow-signUp hover:bg-opacity-90 rounded-full transition duration-300 ease-in-out'
-                >
-                  Contact Me
-                </a>
-                <a
-                  href='#services'
-                  className='text-base xs:text-center font-semibold text-white bg-black py-3 px-8 hover:shadow-signUp hover:bg-opacity-90 rounded-full transition duration-300 ease-in-out'
-                >
-                  Learn More
-                </a>
+                <p className="max-w-[470px] font-medium text-body-color text-base leading-relaxed mb-11">
+                  Decade-Long Software Expert Empowering Teams with Inspiring
+                  Leadership and Cutting-Edge Technologies
+                </p>
+                <div className="flex items-center">
+                  <a
+                    href="#contact"
+                    className="text-base xs:text-center font-semibold text-white bg-primary py-3 px-8 mr-4 hover:shadow-signUp hover:bg-opacity-90 rounded-full transition duration-300 ease-in-out"
+                  >
+                    Contact Me
+                  </a>
+                  <a
+                    href="#services"
+                    className="text-base xs:text-center font-semibold text-white bg-black py-3 px-8 hover:shadow-signUp hover:bg-opacity-90 rounded-full transition duration-300 ease-in-out"
+                  >
+                    Learn More
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div className="w-full lg:w-1/2 self-end px-4">
+              <div className="relative mt-12 lg:mt-0 z-10 2xl:absolute 2xl:bottom-0 2xl:right-40">
+                <Image
+                  src="/images/hero/hero-image-derek.png"
+                  alt="image"
+                  className="max-w-full mx-auto"
+                  width={500}
+                  height={500}
+                  sizes="(max-width: 767px) 100vw, (max-width: 991px) 480px, 600px"
+                />
               </div>
             </div>
           </div>
-          <div className='w-full lg:w-1/2 self-end px-4'>
-            <div className='relative mt-12 lg:mt-0 z-10 2xl:absolute 2xl:bottom-0 2xl:right-40'>
-              <Image
-                src='/images/hero/hero-image-derrick.png'
-                alt='image'
-                className='max-w-full mx-auto'
-                width={500}
-                height={500}
-                sizes='(max-width: 767px) 100vw, (max-width: 991px) 480px, 600px'
-              />
-            </div>
-          </div>
         </div>
-      </div>
 
-      <div>
-        <span className='absolute right-0 bottom-0 -z-1'>
-          <svg
-            width='491'
-            height='350'
-            viewBox='0 0 491 490'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-            className='xs:h-[300px] sm:h-9 md:h-[425px] lg:h-[490px]'
-          >
-            <circle
-              cx='356.5'
-              cy='356.5'
-              r='356.5'
-              fill='url(#paint0_linear_6:37)'
-            />
-            <defs>
-              <linearGradient
-                id='paint0_linear_6:37'
-                x1='356.5'
-                y1='0'
-                x2='356.5'
-                y2='713'
-                gradientUnits='userSpaceOnUse'
-              >
-                <stop stopColor='#4A6CF7' />
-                <stop
-                  offset='1'
-                  stopColor='#1E3BB3'
-                />
-              </linearGradient>
-            </defs>
-          </svg>
-        </span>
-      </div>
-    </section>
+        <div>
+          <span className="absolute right-0 bottom-0 -z-1">
+            <svg
+              width="491"
+              height="350"
+              viewBox="0 0 491 490"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              className="xs:h-[300px] sm:h-9 md:h-[425px] lg:h-[490px]"
+            >
+              <circle
+                cx="356.5"
+                cy="356.5"
+                r="356.5"
+                fill="url(#paint0_linear_6:37)"
+              />
+              <defs>
+                <linearGradient
+                  id="paint0_linear_6:37"
+                  x1="356.5"
+                  y1="0"
+                  x2="356.5"
+                  y2="713"
+                  gradientUnits="userSpaceOnUse"
+                >
+                  <stop stopColor="#4A6CF7" />
+                  <stop offset="1" stopColor="#1E3BB3" />
+                </linearGradient>
+              </defs>
+            </svg>
+          </span>
+        </div>
+      </section>
     </>
-    
   );
 };
 
--- pages/api/educationItems.js
+++ pages/api/educationItems.js
@@ -4,7 +4,19 @@ export default function handler(req, res) {
       id: 1,
       company: "Easton Consulting Technologies",
       title: "Technical Team Lead & Senior Software Engineer",
-      date: "September 2023 - Present",
+      date: "July 2024 - October 2024",
+      summary: `As a Senior Full-Stack Developer and Technical Team Lead, I managed two development teams consisting of MERN/GRAND and LAMP 
+      engineers, overseeing eight high-revenue client projects. I led the seamless migration of a MERN application to a GRAND stack, 
+      ensuring smooth end-to-end functionality. In addition to building a new MERN application for a major insurance company with seamless 
+      Next.js integration for RESTful API usage, I spearheaded the creation of an automated Lambda function that logged JSON events to an 
+      S3 bucket via CloudWatch. Throughout my tenure, I also focused on coaching and mentoring team members, 
+      enhancing their skills to become versatile professionals capable of taking on greater responsibilities.`,
+    },
+    {
+      id: 2,
+      company: "Easton Consulting Technologies",
+      title: "Technical Team Lead & Senior Software Engineer",
+      date: "September 2023 - April 2024",
       summary: `As the Technical Team Lead, I drive success within a fully remote environment by overseeing the development of 
       7+ high-revenue-driving web products. My role involves coaching and mentoring the team, ensuring their growth and success. 
       I lead by example, conducting thorough reviews of Pull Requests (PRs) on Github to maintain the utmost code quality standards. 
@@ -13,7 +25,7 @@ export default function handler(req, res) {
       Additionally, I have written custom WordPress plugins and served as the go-to person for all technology-related requests from the VP of Technology and the CEO.`,
     },
     {
-      id: 2,
+      id: 3,
       company: "Built Technologies",
       title: "Senior Software Engineer",
       date: "January 2023 - July 2023",
@@ -23,7 +35,7 @@ export default function handler(req, res) {
             I ensure compliance with WCAG AA accessibility standards and contribute to continuous deployment and AC changes through a Go CI/CD pipeline.`,
     },
     {
-      id: 3,
+      id: 4,
       company: "DEPT",
       title: "Software Engineer",
       date: "March 2021 - October 2022",
@@ -33,7 +45,7 @@ export default function handler(req, res) {
             I ensured compliance with WCAG AA accessibility standards and contributed to continuous deployment and iterative design changes through a CI/CD pipeline.`,
     },
     {
-      id: 4,
+      id: 5,
       company: "Fresenius Medical Care",
       title: "Senior UI/UX Developer",
       date: "December 2015 - March 2021",
@@ -42,7 +54,7 @@ export default function handler(req, res) {
         Additionally, I resolved security vulnerabilities identified through penetration testing, ensuring compliance with WCAG AA accessibility standards. My collaborative skills were put to use in Agile Scrum teams, contributing to efficient teamwork.`,
     },
     {
-      id: 5,
+      id: 6,
       company: "Fidelity Investments",
       title: "Senior UI/UX Developer",
       date: "July 2014 - November 2015",
@@ -50,7 +62,7 @@ export default function handler(req, res) {
         Compliance with WCAG AA accessibility standards was a priority, and I fostered camaraderie and collaboration within teams to enhance productivity. Additionally, I developed a full-stack MEAN solution for an Executive Vice President, streamlining the management of business data.`,
     },
     {
-      id: 6,
+      id: 7,
       company: "Medullan, Inc.",
       title: "Lead UI/UX Developer",
       date: "June 2013 - May 2014",
@@ -59,7 +71,7 @@ export default function handler(req, res) {
         Additionally, I specialized in creating custom WordPress solutions that were meticulously tailored to cater to the specific requirements of clients.`,
     },
     {
-      id: 7,
+      id: 8,
       company: "Southern New Hampshire University",
       title: "Bachelors of Computer Science",
       date: "2020",
